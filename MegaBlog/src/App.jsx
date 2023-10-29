2f5e3b16c1d76d57668b1e5419ca8d5cf0a163a4
jofin15 <70406065+jofin15@users.noreply.github.com>
2023-10-29 17:24:48 +0000

    new

--- MegaBlog/src/App.jsx
+++ MegaBlog/src/App.jsx
@@ -1,15 +1,37 @@
-
+import { useEffect, useState } from 'react'
 import './App.css'
+import "./index.css"
+import {Header,Footer} from "./components/index"
+import {useDispatch} from "react-redux"
+import authService from './appwrite/auth'
+import {login,logout} from "./store/authSlice"
+
 
 function App() {
-  
+  const [loading,setLoading]=useState(true)
+  const dispatch=useDispatch()
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <div>
-    
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+        console.log(userData);
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>setLoading(false))
+  },[])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <Footer />
+      </div>
     </div>
-  )
+  ):null
 }
 
 export default App

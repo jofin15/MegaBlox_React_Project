8922c37323255f98494be96bb10989ed2f220264
jofin15 <70406065+jofin15@users.noreply.github.com>
2023-10-31 06:51:12 +0000

    new

--- MegaBlog/src/components/Header/Header.jsx
+++ MegaBlog/src/components/Header/Header.jsx
@@ -32,7 +32,7 @@ const navItems=[
   }
 ]
   return (
-    <div className='py-3 shadow bg-gray-500'>
+    <header className='py-3 shadow bg-gray-500'>
       <Container>
         <nav className='flex'>
           <div className='mr-4'>
@@ -40,9 +40,28 @@ const navItems=[
               <Logo  width='70px'/>
             </Link>
           </div>
+          <ul className='flex'>
+            {
+              navItems.map((item)=>item.active ? (
+                <li key={item.name}>
+                  <button onClick={()=>navigate(item.slug)}  
+                  className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
+                    {item.name}</button>
+                </li>
+              ):null)
+            }
+            {
+              authStatus && (
+                <li>
+                  <LogoutBtn />
+                </li>
+              )
+            }
+          </ul>
         </nav>
-      </Container>
-    </div>
+        </ Container >
+      </header>
+    
   )
 }
 

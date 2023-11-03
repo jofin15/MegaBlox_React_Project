40563b78e65e3e99c46eae4d028be11a1ff3adcf
jofin15 <70406065+jofin15@users.noreply.github.com>
2023-11-03 06:22:06 +0000

    asdf

--- MegaBlog/src/components/index.js
+++ MegaBlog/src/components/index.js
@@ -3,4 +3,7 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import LogoutBtn from "./Header/LogoutBtn"
 import Logo from "./Logo";
-export {Header,Footer,Container,Logo,LogoutBtn} 
+import Input from "./Input";
+import Button from "../components/Header/Button"
+
+export {Header,Footer,Container,Logo,LogoutBtn,Input,Button} 

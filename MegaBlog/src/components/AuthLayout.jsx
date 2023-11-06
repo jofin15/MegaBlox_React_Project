48c83b582b84e1371ebd97c8f9a6f155d2b9a92a
jofin15 <70406065+jofin15@users.noreply.github.com>
2023-11-06 19:09:54 +0000

    sdf

---
+++ MegaBlog/src/components/AuthLayout.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+export default function Protected({children,authentication=true}) {
+    const navigate=useNavigate()
+    const [loader,setLoader]=useState(true)
+    const authStatus=useSelector(state=>state.auth.status)
+
+    useEffect(()=>{
+
+    },[authStatus,navigate,authentication])
+    return (
+    <div>AuthLayout</div>
+  )
+}
+

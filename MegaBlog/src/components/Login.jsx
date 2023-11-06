57d467c1d38393b692c5cd2541d4efd8aed0fab8
jofin15 <70406065+jofin15@users.noreply.github.com>
2023-11-06 11:19:10 +0000

    asdf

--- MegaBlog/src/components/Login.jsx
+++ MegaBlog/src/components/Login.jsx
@@ -6,6 +6,7 @@ import { useDispatch } from 'react-redux'
 import authService  from '../appwrite/auth'
 import {useForm} from "react-hook-form"
 import Input from "../components/Input"
+
 function Login() {
     const navigate=useNavigate()
     const dispatch=useDispatch()
